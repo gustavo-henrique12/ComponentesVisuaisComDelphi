eb195bc95f27aed0c56d678dc1b9bdff82e773c6
Gustavo <gustavodossantos764@gmail.com>
2022-12-17 09:38:49 -0400

    Adicionado Form com alguns controles Visuai Delphi

---
+++ uFrmPrincipal.pas
@@ -0,0 +1,66 @@
+unit uFrmPrincipal;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, ComCtrls, StdCtrls, ImgList;
+
+type
+  TFrmPrincipal = class(TForm)
+    PnlTitulo: TPanel;
+    PgCntrlPrincipal: TPageControl;
+    ts1: TTabSheet;
+    ts2: TTabSheet;
+    ts3: TTabSheet;
+    grp1: TGroupBox;
+    Label1: TLabel;
+    edtCaracteres: TEdit;
+    BtnExibirDataHora: TButton;
+    il1: TImageList;
+    procedure FormShow(Sender: TObject);
+    procedure edtCaracteresEnter(Sender: TObject);
+    procedure edtCaracteresExit(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmPrincipal: TFrmPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFrmPrincipal.FormShow(Sender: TObject);
+begin
+   PnlTitulo.Caption := 'Mudando o Caption do Panel com onShow';
+   PnlTitulo.Color := $00B9FFFF;
+end;
+
+procedure TFrmPrincipal.edtCaracteresEnter(Sender: TObject);
+begin
+   edtCaracteres.Color := $00b9ffff;
+end;
+
+procedure TFrmPrincipal.edtCaracteresExit(Sender: TObject);
+begin
+   if edtCaracteres.Text = ' ' then
+   begin
+      MessageDlg('Você não digitou nenhum conteúdo', mtError, [mbOK], 0);
+      edtCaracteres.SetFocus;
+   end
+   else
+      if Length(edtCaracteres.text)<> 4 then
+      begin
+         MessageDlg('Você não digitou apenas 4 caracteres', mtError, [mbOK], 0);
+         edtCaracteres.SetFocus;
+         edtCaracteres.SelectAll;
+      end
+      else
+         edtCaracteres.Color := clWindow;
+end;
+
+end.
